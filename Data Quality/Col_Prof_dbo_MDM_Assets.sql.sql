dc34d4427f06c5732ba121bb651d4294cf136f80
tvillarreal-key <tvillarreal@keyenergy.com>
2024-01-12 21:24:13 -0600

    Added script

--- Data Quality/Col_Prof_dbo_MDM_Assets.sql.sql
+++ Data Quality/Col_Prof_dbo_MDM_Assets.sql.sql
@@ -12,10 +12,12 @@ SELECT @query = @query + 'SELECT
     ''' + CAST(ORDINAL_POSITION AS VARCHAR(100)) + ''' AS ordinal_position, 
     ''' + IS_NULLABLE + ''' AS is_nullable, 
     ''' + DATA_TYPE + ''' AS data_type,  
-    (CASE WHEN ''' + DATA_TYPE + ''' = ''nvarchar'' THEN MAX (CAST(LEN([' + COLUMN_NAME + ']) AS VARCHAR(200))) ELSE '''' END) AS max_len,      
+    (CASE WHEN ''' + DATA_TYPE + ''' = ''nvarchar'' THEN MAX (CAST(LEN([' + COLUMN_NAME + ']) AS VARCHAR(200))) ELSE ''0'' END) AS max_len,   
+    COUNT(CASE WHEN [' + COLUMN_NAME + '] IS NULL THEN 1 ELSE 0 END) AS num_values,        
     COUNT(DISTINCT [' + COLUMN_NAME + ']) AS num_unique_values, 
-    COUNT([' + COLUMN_NAME + ']) AS num_values, 
+    CAST(COUNT(DISTINCT [' + COLUMN_NAME + ']) * 1.0 / COUNT(CASE WHEN [' + COLUMN_NAME + '] IS NULL THEN 1 ELSE 0 END)*100 AS DECIMAL(5,2)) AS pct_unique_values,  
     SUM(CASE WHEN [' + COLUMN_NAME + '] IS NULL THEN 1 ELSE 0 END) AS num_null_values, 
+    CAST(SUM(CASE WHEN [' + COLUMN_NAME + '] IS NULL THEN 1 ELSE 0 END) * 1.0 / COUNT(CASE WHEN [' + COLUMN_NAME + '] IS NULL THEN 1 ELSE 0 END)*100 AS DECIMAL(10,2)) AS pct_null_values,        
     MIN (CAST([' + COLUMN_NAME + '] AS VARCHAR(200))) AS min_value, 
     MAX (CAST([' + COLUMN_NAME + '] AS VARCHAR(200))) AS max_value
 FROM [' + @table_catalog + '].[' + @table_schema + '].[' + @table_name + ']
