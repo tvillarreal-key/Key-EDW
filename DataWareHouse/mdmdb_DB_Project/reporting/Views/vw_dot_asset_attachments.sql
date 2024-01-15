3836fc08588a59e3de227175f5c77c165f112679
tvillarreal-key <tvillarreal@keyenergy.com>
2024-01-15 14:56:08 -0600

    Added kex_expiration_datetime >= getdate() to VIEW [reporting].[vw_dot_asset_attachments]

--- DataWareHouse/mdmdb_DB_Project/reporting/Views/vw_dot_asset_attachments.sql
+++ DataWareHouse/mdmdb_DB_Project/reporting/Views/vw_dot_asset_attachments.sql
@@ -1,7 +1,12 @@
-CREATE VIEW reporting.vw_dot_asset_attachments AS
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- DROP VIEW [reporting].[vw_dot_asset_attachments];
+CREATE VIEW [reporting].[vw_dot_asset_attachments] AS
 SELECT  document_id, default_attachment, file_name, file_type, asset_num, 
         kex_expiration_datetime, [name], notes, parent_asset_num, [path], 
         restriction, type_id
 FROM reporting.D365_AssetAttachments
+where kex_expiration_datetime >= getdate()
 GO
-
