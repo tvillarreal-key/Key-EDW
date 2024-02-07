de4bd219f86cef694e4af799f6eaa77a5099116d
tvillarreal-key <tvillarreal@keyenergy.com>
2024-02-07 10:56:25 -0600

    Added upload_date to view definition

--- DataWareHouse/mdmdb_DB_Project/reporting/Views/vw_dot_dvir.sql
+++ DataWareHouse/mdmdb_DB_Project/reporting/Views/vw_dot_dvir.sql
@@ -1,10 +1,14 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- Drop VIEW [reporting].[vw_dot_dvir] 
 CREATE VIEW [reporting].[vw_dot_dvir] AS
 SELECT  report_number, report_date, updated, [version], observer, observer_emp_num, duration, 
         latitude, longitude, temperature, wind_speed, weather, score_percent, marketplace, lco, 
         district_manager, supervisor, inspection_type, asset_num,is_trailer_pulled, trailer_asset_num, 
         reparir_cost_greater_than_50_dollars_or_repair_time_greater_than_1_hour, can_repairs_be_made_by_driver, 
         repairs_made_by_driver, driver_name, defects_corrected, deffect_correction_date, work_order_id
-        ,link
+        ,link, upload_date
 FROM reporting.IScout_159094
 GO
-
