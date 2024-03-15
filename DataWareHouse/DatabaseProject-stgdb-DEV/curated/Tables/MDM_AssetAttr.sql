0efe759922c6c0d38374f10bb552c51ee53850a1
tvillarreal-key <tvillarreal@keyenergy.com>
2024-03-15 15:01:47 -0500

    updated curated and invalidated tables

--- DataWareHouse/DatabaseProject-stgdb-DEV/curated/Tables/MDM_AssetAttr.sql
+++ DataWareHouse/DatabaseProject-stgdb-DEV/curated/Tables/MDM_AssetAttr.sql
@@ -2,23 +2,27 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-DROP TABLE IF EXISTS [curated].[MDM_AssetAttr]
+DROP TABLE IF EXISTS [curated].[MDM_AssetAttr] 
 GO
 CREATE TABLE [curated].[MDM_AssetAttr](
-    [AssetsHash]              NVARCHAR(32)   NOT NULL,
-    [StagingLoadTimestamp]    DATETIME2 (7)  NOT NULL,
-    [StagingSourceSystem]     NVARCHAR (50)  NOT NULL,
-    [RawLoadTimestamp]        DATETIME2 (7)  NOT NULL,
-    [EffectiveStartDate]      DATE           NULL,
-    [EffectiveEndDate]        DATE           NULL,
-    [CuratedLoadTimestamp]    DATETIME2 (7)  NOT NULL,
-    [CuratedTransformedBy]    NVARCHAR (200) NOT NULL,
-	[Asset_ID] 				  INT	 		 NOT NULL,
-	[Asset_Num] 			  NVARCHAR (60)  NOT NULL,
-	[AttrCode] 				  NVARCHAR (50)  NOT NULL,
-	[AttrValue] 			  NVARCHAR (max) NULL,
-    CONSTRAINT [PK_MDM_AssetAttr] PRIMARY KEY NONCLUSTERED
-    (
-        [Asset_ID], [Asset_Num], [AttrCode] ASC
-    )
-)
+	[AssetAttrHash] [nvarchar](32) NOT NULL,
+	[StagingLoadTimestamp] [datetime2](7) NOT NULL,
+	[StagingSourceSystem] [nvarchar](50) NOT NULL,
+	[RawLoadTimestamp] [datetime2](7) NOT NULL,
+	[EffectiveStartDate] [date] NULL,
+	[EffectiveEndDate] [date] NULL,
+	[CuratedLoadTimestamp] [datetime2](7) NOT NULL,
+	[CuratedTransformedBy] [nvarchar](200) NOT NULL,
+	[Asset_ID] [int] NOT NULL,
+	[Asset_Num] [nvarchar](60) NOT NULL,
+	[AttrCode] [nvarchar](50) NOT NULL,
+	[AttrValue] [nvarchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+ALTER TABLE [curated].[MDM_AssetAttr] ADD  CONSTRAINT [PK_MDM_AssetAttr] PRIMARY KEY NONCLUSTERED 
+(
+	[AssetAttrHash] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
