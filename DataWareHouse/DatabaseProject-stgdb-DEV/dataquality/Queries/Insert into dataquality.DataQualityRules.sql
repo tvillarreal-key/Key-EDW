14a35514f312b9506e8bd8e3e291af431de198cd
tvillarreal-key <tvillarreal@keyenergy.com>
2024-04-29 16:01:51 -0500

    Update DQ_Rules and insert query

--- DataWareHouse/DatabaseProject-stgdb-DEV/dataquality/Queries/Insert into dataquality.DataQualityRules.sql
+++ DataWareHouse/DatabaseProject-stgdb-DEV/dataquality/Queries/Insert into dataquality.DataQualityRules.sql
@@ -1,50 +1,101 @@
 USE [stgdb-DEV]
 GO
 
-INSERT INTO [dataquality].[DataQualityRules]
+INSERT INTO [dataquality].[DQ_Rules]
            ([RuleName]
            ,[RuleType]
+           ,[DatabaseName]
+           ,[SchemaName]
+           ,[TableName]
+           ,[ColumnName]
            ,[RuleDefinition]
            ,[ExpectedResult]
            ,[IsActive]
            ,[Severity])
      VALUES
-           ('Age - Data Range Check'
-           ,'Data Range Check'
-		   ,'''Age &gt;= 0 AND Age &lt;= 100'''
-		   ,'TotalRowCount'
-		   ,'1'
-		   ,'3')
+           ('Asset_Desc Null Check'
+           ,'Null Check'
+           ,'stgdb-DEV'
+           ,'staging'
+           ,'MDM_Assets'
+           ,'Asset_Desc'
+           ,'IS NULL'
+           ,0
+           ,1
+           ,3)
 GO
-INSERT INTO [dataquality].[DataQualityRules]
+INSERT INTO [dataquality].[DQ_Rules]
            ([RuleName]
            ,[RuleType]
+           ,[DatabaseName]
+           ,[SchemaName]
+           ,[TableName]
+           ,[ColumnName]
            ,[RuleDefinition]
            ,[ExpectedResult]
            ,[IsActive]
            ,[Severity])
      VALUES
-           ('Email - Pattern Check'
-           ,'Pattern Check'
-		   ,'''Email LIKE ''%@%.%'''''
-		   ,'TotalRowCount'
-		   ,'1'
-		   ,'2')
+           ('ID Null Check'
+           ,'Null Check'
+           ,'stgdb-DEV'
+           ,'staging'
+           ,'MDM_Assets'
+           ,'ID'
+           ,'IS NULL'
+           ,0
+           ,1
+           ,2)
+
 GO
-INSERT INTO [dataquality].[DataQualityRules]
+INSERT INTO [dataquality].[DQ_Rules]
            ([RuleName]
            ,[RuleType]
+           ,[DatabaseName]
+           ,[SchemaName]
+           ,[TableName]
+           ,[ColumnName]
            ,[RuleDefinition]
            ,[ExpectedResult]
            ,[IsActive]
            ,[Severity])
      VALUES
-           ('Start Date - Consistency Check'
-           ,'Consistency Check'
-		   ,'''StartDate &lt; EndDate'''
-		   ,'TotalRowCount'
-		   ,'1'
-		   ,'1')
+           ('Asset_Class Null Check'
+           ,'Null Check'
+           ,'stgdb-DEV'
+           ,'staging'
+           ,'MDM_Assets'
+           ,'Asset_Class'
+           ,'IS NULL'
+           ,0
+           ,1
+           ,2)
+
 GO
+INSERT INTO [dataquality].[DQ_Rules]
+           ([RuleName]
+           ,[RuleType]
+           ,[DatabaseName]
+           ,[SchemaName]
+           ,[TableName]
+           ,[ColumnName]
+           ,[RuleDefinition]
+           ,[ExpectedResult]
+           ,[IsActive]
+           ,[Severity])
+     VALUES
+           ('ID Is Numeric Check'
+           ,'Numeric Check'
+           ,'stgdb-DEV'
+           ,'staging'
+           ,'MDM_Assets'
+           ,''
+           ,'ISNUMERIC(Asset_Num) = 0'
+           ,0
+           ,1
+           ,1)
+
+GO
+
 
 
--- DataWareHouse/DatabaseProject-stgdb-DEV/dataquality/Tables/DQ_Rules.sql
+++ DataWareHouse/DatabaseProject-stgdb-DEV/dataquality/Tables/DQ_Rules.sql
@@ -2,29 +2,21 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-DROP TABLE IF EXISTS [dataquality].[DQ_Rules] 
+DROP TABLE IF EXISTS [dataquality].[DQ_Rules]
 GO
-CREATE TABLE [dataquality].[DQ_Rules]  (
-    ID INT IDENTITY(1,1),
-    RuleName VARCHAR(255),
-    RuleType VARCHAR(255),
-    DatabaseName VARCHAR(255),
-    SchemaName VARCHAR(255),
-    TableName VARCHAR(255),
-    ColumnName VARCHAR(255),
-    RuleDefinition VARCHAR(255),
-    ExpectedResult INT,
-    IsActive INT,
-    Severity INT
-);
+CREATE TABLE [dataquality].[DQ_Rules](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[RuleName] [varchar](255) NULL,
+	[RuleType] [varchar](255) NULL,
+	[DatabaseName] [varchar](255) NULL,
+	[SchemaName] [varchar](255) NULL,
+	[TableName] [varchar](255) NULL,
+	[ColumnName] [varchar](255) NULL,
+	[RuleDefinition] [varchar](255) NULL,
+	[ExpectedResult] [int] NULL,
+	[IsActive] [int] NULL,
+	[Severity] [int] NULL,
+    [LoadDate] DATETIME2 DEFAULT GETDATE()
+) ON [PRIMARY]
+GO
 
-INSERT INTO [dataquality].[DQ_Rules] (RuleName, RuleType, DatabaseName, SchemaName, TableName, ColumnName, RuleDefinition, ExpectedResult, IsActive, Severity)
-VALUES 
-('ID Null Check', 'Null Check', 'stgdb-DEV', 'staging', 'MDM_Assets', 'Asset_Num', 'ID IS NULL', 0, 1, 2),
-('Asset_Desc Null Check', 'Null Check', 'stgdb-DEV', 'staging', 'MDM_Assets', 'Asset_Desc', 'Asset_Desc IS NULL', 0, 1, 2),
-('Asset_Class Null Check', 'Null Check', 'stgdb-DEV', 'staging', 'MDM_Assets', 'Asset_Class', 'Asset_Class IS NULL', 0, 1, 2),
-('ID Is Numeric Check', 'Numeric Check', 'stgdb-DEV', 'staging', 'MDM_Assets', 'Asset_Num', 'ISNUMERIC(ID) = 0', 0, 1, 2);
-
- SELECT
- *
- FROM [dataquality].[DQ_Rules];
