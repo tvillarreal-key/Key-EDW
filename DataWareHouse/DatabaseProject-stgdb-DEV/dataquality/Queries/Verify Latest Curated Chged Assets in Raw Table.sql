1c5f8441609cfe47a5c336d52064797d15830329
tvillarreal-key <tvillarreal@keyenergy.com>
2024-05-07 10:16:17 -0500

    Updated latest curated changed assets list

--- DataWareHouse/DatabaseProject-stgdb-DEV/dataquality/Queries/Verify Latest Curated Chged Assets in Raw Table.sql
+++ DataWareHouse/DatabaseProject-stgdb-DEV/dataquality/Queries/Verify Latest Curated Chged Assets in Raw Table.sql
@@ -1,15 +1,9 @@
 -- Finds the raw rows for the last changed assets in curated load
 SELECT *
-FROM [raw].[MDM_Assets] A
-WHERE A.Asset_Num IN
+FROM [curated].[MDM_Assets] B
+WHERE LoadDate IN
 (
-    SELECT Asset_Num
-    FROM [curated].[MDM_Assets] B
-    WHERE LoadDate IN
-    (
-        SELECT MAX(LoadDate)
-        FROM [curated].[MDM_Assets] C
-    )
+    SELECT MAX(LoadDate)
+    FROM [curated].[MDM_Assets] C
 )
-ORDER BY Asset_Num ASC, LoadDate DESC
 ;
