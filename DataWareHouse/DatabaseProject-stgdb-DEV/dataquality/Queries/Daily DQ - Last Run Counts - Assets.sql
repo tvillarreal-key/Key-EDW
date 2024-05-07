7961ef94f10bb306fb7499a4cf39caffaab1ff7f
tvillarreal-key <tvillarreal@keyenergy.com>
2024-05-07 10:46:25 -0500

    Updated Last Run Counts

--- DataWareHouse/DatabaseProject-stgdb-DEV/dataquality/Queries/Daily DQ - Last Run Counts - Assets.sql
+++ DataWareHouse/DatabaseProject-stgdb-DEV/dataquality/Queries/Daily DQ - Last Run Counts - Assets.sql
@@ -11,3 +11,41 @@ SELECT [LogSequence]
         FROM [metricsvault].[Integration_AuditLog]        
     )
     ORDER BY 3, 2 ASC
+    ;
+
+-- MDM_Assets
+SELECT 'staging data MDM_Assets cnts: ' cnt_type
+      ,FORMAT(CAST(COUNT(*) AS money), 'N0') AS cnt
+FROM [staging].[MDM_Assets] A
+UNION
+SELECT 'raw data MDM_Assets cnts: ' cnt_type
+      ,FORMAT(CAST(COUNT(*) AS money), 'N0') AS cnt
+FROM [raw].[MDM_Assets] B
+WHERE LoadDate IN
+(
+    SELECT MAX(LoadDate)
+    FROM [raw].[MDM_Assets] C
+)
+UNION
+SELECT 'curated data MDM_Assets cnts: ' cnt_type
+      ,FORMAT(CAST(COUNT(*) AS money), 'N0') AS cnt
+FROM [curated].[MDM_Assets] B
+;
+-- MDM_AssetAttr
+SELECT 'staging data MDM_AssetAttr cnts: ' cnt_type
+      ,FORMAT(CAST(COUNT(*) AS money), 'N0') AS cnt
+FROM [staging].[MDM_AssetAttr] A
+UNION
+SELECT 'raw data MDM_AssetAttr cnts: ' cnt_type
+      ,FORMAT(CAST(COUNT(*) AS money), 'N0') AS cnt
+FROM [raw].[MDM_AssetAttr] B
+WHERE LoadDate IN
+(
+    SELECT MAX(LoadDate)
+    FROM [raw].[MDM_AssetAttr] C
+)
+UNION
+SELECT 'curated data MDM_AssetAttr cnts: ' cnt_type
+      ,FORMAT(CAST(COUNT(*) AS money), 'N0') AS cnt
+FROM [curated].[MDM_AssetAttr] B
+;
