8882885ce98e81118d5ff0d7a1e73ed552f38259
tvillarreal-key <tvillarreal@keyenergy.com>
2024-05-10 16:55:35 -0500

    Cleaning up change views

--- DataWareHouse/DatabaseProject-ElasticMasterDB/datavault/Views/vw_changed_hubasset_recs.sql
+++ DataWareHouse/DatabaseProject-ElasticMasterDB/datavault/Views/vw_changed_hubasset_recs.sql
@@ -2,6 +2,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+-- EXTERNAL VIEW
+--USE ElasticMasterDB;
 DROP VIEW IF EXISTS [datavault].[vw_changed_hubasset_recs] 
 GO
 -- EXTERNAL VIEW
@@ -18,10 +20,21 @@ FROM [staging].[MDM_Assets] A
 LEFT JOIN [datavault].[HubAsset] B
 ON A.Asset_Num = B.Asset_Num
 WHERE B.Asset_Num IS NULL -- This gets the new records
-/* No hub records are updated, only inserted
-   The use of a nonclustered primary key is strongly recommended because the hash key is used 
-   as the primary key.
-   The lookup (vw_changed_hubxx_recs view) is performed using the business key and not the hash key. 
+-- No hub records are updated, only inserted
+/* Always do the hub lookups using the business key to avoid the rare risk
+     of hash collisions. If two business keys produce the same hash key, 
+     a lookup on the hash key would return that the hash key already exists in the target. 
+     But due to the hash collision, this would mean a different business key.
+     However, in the case of a hash collision, this will result in a primary key constraint
+     violation when inserting the record in the next step. This is not the preferred solution,
+     but it is the desired one, because we’d like to be notified in case of collision.
 */
 ;
 GO
+-------------
+-- testing
+-------------
+--USE ElasticMasterDB;
+SELECT *
+FROM [datavault].[vw_changed_hubasset_recs]
+;
