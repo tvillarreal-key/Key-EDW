1f5fa7641ccae38b93b2193b28f6b25a0fa8099b
tvillarreal-key <tvillarreal@keyenergy.com>
2024-05-10 16:23:06 -0500

    Updated hubasset

--- DataWareHouse/DatabaseProject-dwhdb-DEV/ElasticMasterDB/Views/vw_changed_hubasset_recs.sql
+++ DataWareHouse/DatabaseProject-dwhdb-DEV/ElasticMasterDB/Views/vw_changed_hubasset_recs.sql
@@ -21,6 +21,14 @@ LEFT JOIN [datavault].[HubAsset] B
 ON A.Asset_Num = B.Asset_Num
 WHERE B.Asset_Num IS NULL -- This gets the new records
 -- No hub records are updated, only inserted
+/* Always do the hub lookups using the business key to avoid the rare risk
+     of hash collisions. If two business keys produce the same hash key, 
+     a lookup on the hash key would return that the hash key already exists in the target. 
+     But due to the hash collision, this would mean a different business key.
+     However, in the case of a hash collision, this will result in a primary key constraint
+     violation when inserting the record in the next step. This is not the preferred solution,
+     but it is the desired one, because we’d like to be notified in case of collision.
+*/
 ;
 GO
 -------------
