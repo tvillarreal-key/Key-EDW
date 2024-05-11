25511dc77cb092ab56d1d33e34a52da61a5fd0a3
tvillarreal-key <tvillarreal@keyenergy.com>
2024-05-11 13:21:10 -0500

    Update changed sat asset recs view to add sat load

--- DataWareHouse/DatabaseProject-ElasticMasterDB/datavault/Views/vw_changed_satasset_recs.sql
+++ DataWareHouse/DatabaseProject-ElasticMasterDB/datavault/Views/vw_changed_satasset_recs.sql
@@ -5,77 +5,74 @@ GO
 DROP VIEW IF EXISTS [datavault].[vw_changed_satasset_recs] 
 GO
 CREATE VIEW [datavault].[vw_changed_satasset_recs] AS
-SELECT 
-    A.*
+SELECT DISTINCT
+    stg.[AssetsHashKey]
+    ,GETDATE() AS SatLoadDate
+    ,stg.[SourceSystem]
+    ,'' AS LoadEndDate
+    ,stg.[AssetsChkSum]
+    ,stg.[LoadDate] AS StgExtractDate
+    ,stg.[LastSeenDate]
+    ,stg.[ID]
+    ,stg.[Asset_Num]
+    ,stg.[Asset_Desc]
+    ,stg.[Asset_Class]
+    ,stg.[Manufacturer]
+    ,stg.[Model]
+    ,stg.[Model_Year]
+    ,stg.[Series]
+    ,stg.[Serial_Num]
+    ,stg.[Status]
+    ,stg.[Status_Desc]
+    ,stg.[Source]
+    ,stg.[Marketed]
+    ,stg.[Revenue_Track]
+    ,stg.[Parent_Asset_Num]
+    ,stg.[Child_Relationship_Type]
+    ,stg.[Loco_Code]
+    ,stg.[Loco_Name]
+    ,stg.[Loca_Code]
+    ,stg.[Loca_Name]
+    ,stg.[MKTP_Code]
+    ,stg.[MKTP_Name]
+    ,stg.[LVL1_Code]
+    ,stg.[LVL1_Name]
+    ,stg.[LOB_Code]
+    ,stg.[LOB_Name]
+    ,stg.[Dist_Code]
+    ,stg.[Dist_Name]
+    ,stg.[Mare_Code]
+    ,stg.[Mare_Name]
+    ,stg.[Yard_Code]
+    ,stg.[Yard_Name]
+    ,stg.[W_Status]
+    ,stg.[Crew_Needed]
+    ,stg.[Avail_Date]
+    ,stg.[LastWT_Date]
+    ,stg.[Mod_Date]
+    ,stg.[Mod_User]
+    ,stg.[Note]
+    ,stg.[Customer]
+    ,stg.[JobType]
+    ,stg.[Supervisor]
+    ,stg.[Operator]
+    ,stg.[Directions]
+    ,stg.[CompanyMan]
+    ,stg.[CompanyManPhone]
+    ,stg.[Daylight24HR]
+    ,stg.[Size]
+    ,stg.[UsedByRigYard]
+    ,stg.[UsedByRigNum]
+    ,stg.[Source_LastUpdateDate]
+    ,stg.[Source_LastUpdateBy]
 ,'I' AS Chg_Flag
-FROM [staging].[MDM_Assets] A 
-LEFT JOIN [datavault].[SatAsset] B
-ON A.AssetsHashKey = B.AssetsHashKey
-WHERE B.AssetsHashKey IS NULL -- This gets the new records
-UNION
-SELECT 
-    A.[AssetsHashKey]
-    ,A.[AssetsChkSum]
-    ,A.[LoadDate]
-    ,A.[SourceSystem]
-    ,A.[LastSeenDate]
-    ,A.[ID]
-    ,A.[Asset_Num]
-    ,A.[Asset_Desc]
-    ,A.[Asset_Class]
-    ,A.[Manufacturer]
-    ,A.[Model]
-    ,A.[Model_Year]
-    ,A.[Series]
-    ,A.[Serial_Num]
-    ,A.[Status]
-    ,A.[Status_Desc]
-    ,A.[Source]
-    ,A.[Marketed]
-    ,A.[Revenue_Track]
-    ,A.[Parent_Asset_Num]
-    ,A.[Child_Relationship_Type]
-    ,A.[Loco_Code]
-    ,A.[Loco_Name]
-    ,A.[Loca_Code]
-    ,A.[Loca_Name]
-    ,A.[MKTP_Code]
-    ,A.[MKTP_Name]
-    ,A.[LVL1_Code]
-    ,A.[LVL1_Name]
-    ,A.[LOB_Code]
-    ,A.[LOB_Name]
-    ,A.[Dist_Code]
-    ,A.[Dist_Name]
-    ,A.[Mare_Code]
-    ,A.[Mare_Name]
-    ,A.[Yard_Code]
-    ,A.[Yard_Name]
-    ,A.[W_Status]
-    ,A.[Crew_Needed]
-    ,A.[Avail_Date]
-    ,A.[LastWT_Date]
-    ,A.[Mod_Date]
-    ,A.[Mod_User]
-    ,A.[Note]
-    ,A.[Customer]
-    ,A.[JobType]
-    ,A.[Supervisor]
-    ,A.[Operator]
-    ,A.[Directions]
-    ,A.[CompanyMan]
-    ,A.[CompanyManPhone]
-    ,A.[Daylight24HR]
-    ,A.[Size]
-    ,A.[UsedByRigYard]
-    ,A.[UsedByRigNum]
-    ,A.[Source_LastUpdateDate]
-    ,A.[Source_LastUpdateBy]
-,'U' AS Chg_Flag
-FROM [staging].[MDM_Assets] A 
-LEFT JOIN [datavault].[SatAsset] B
-ON A.AssetsHashKey = B.AssetsHashKey
-WHERE A.AssetsChkSum != B.AssetsChkSum -- This gets the updated records
+FROM [staging].[MDM_Assets] stg 
+LEFT JOIN [datavault].[SatAsset] sat
+ON (stg.AssetsHashKey = sat.AssetsHashKey
+    AND sat.LoadEndDate IS NULL)
+WHERE 
+    (sat.AssetsChkSum IS NULL 
+     OR stg.AssetsChkSum != sat.AssetsChkSum) -- This gets the updated records
 ;
 GO
 
