82eeed1827783dc4f7ae85b70f2f34677a819d8b
tvillarreal-key <tvillarreal@keyenergy.com>
2024-05-29 15:14:51 -0500

    dims

--- DataWareHouse/DatabaseProject-dwhdb-DEV/datamart/dimLocation.sql
+++ DataWareHouse/DatabaseProject-dwhdb-DEV/datamart/dimLocation.sql
@@ -2,21 +2,26 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-DROP TABLE IF EXISTS [datavault].[HubAsset] 
-GO
-CREATE TABLE [datavault].[HubAsset](
-	[AssetsHashKey] [nvarchar](32) NOT NULL,
+CREATE TABLE [datamart].[dimLocation](
+	[LocationHashKey] [nvarchar](32) NOT NULL,
+	[LocationChkSum] [nvarchar](32) NOT NULL,
 	[LoadDate] [datetime2](7) NOT NULL,
 	[SourceSystem] [nvarchar](50) NOT NULL,
-	[Asset_Num] [nvarchar](60) NOT NULL,
-	[LastSeenDate] [datetime2](7) NOT NULL,
-CONSTRAINT [PK_HubAsset] PRIMARY KEY NONCLUSTERED 
-(
-	[AssetsHashKey] ASC
-) ON [PRIMARY],
-CONSTRAINT [UK_HubAsset] UNIQUE NONCLUSTERED 
-(
-	[Asset_Num] ASC
-) ON [PRIMARY],
+	[LastSeenDate] [datetime2](7) NULL,
+	[Yard_Code] [nvarchar](20) NULL,
+	[Yard_Name] [nvarchar](100) NULL,
+	[Loco_Code] [nvarchar](20) NULL,
+	[Loco_Name] [nvarchar](100) NULL,
+	[Loca_Code] [nvarchar](20) NULL,
+	[Loca_Name] [nvarchar](100) NULL,
+	[MKTP_Code] [nvarchar](20) NULL,
+	[MKTP_Name] [nvarchar](100) NULL
 ) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+ALTER TABLE [datamart].[dimLocation] ADD  CONSTRAINT [PK_dimLocation] PRIMARY KEY NONCLUSTERED 
+(
+	[LocationHashKey] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
---
+++ DataWareHouse/DatabaseProject-stgdb-DEV/curated/Tables/dimLOB.sql
@@ -0,0 +1,27 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+DROP TABLE IF EXISTS [curated].[dimLOB] 
+GO
+CREATE TABLE [curated].[dimLOB](
+	[LOBHashKey] [nvarchar](32) NOT NULL,
+	[LOBChkSum] [nvarchar](32) NOT NULL,
+	[LoadDate] [datetime2](7) NOT NULL,
+	[SourceSystem] [nvarchar](50) NOT NULL,
+	[LastSeenDate] [datetime2](7) NULL,
+    [Yard_Code] [nvarchar](20) NULL,
+    [Yard_Name] [nvarchar](100) NULL, 
+    [Lvl1_Code] [nvarchar](20) NULL,
+    [Lvl1_Name] [nvarchar](100) NULL,
+    [Lob_Code] [nvarchar](20) NULL,
+    [Lob_Name] [nvarchar](100) NULL,     
+) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+ALTER TABLE [curated].[dimLOB] ADD  CONSTRAINT [PK_dimLOB] PRIMARY KEY NONCLUSTERED 
+(
+	[LOBHashKey] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO 
