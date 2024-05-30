e21e225def5e35534ca02dbe665a6399f4f9272b
tvillarreal-key <tvillarreal@keyenergy.com>
2024-05-30 15:45:51 -0500

    xxx

--- DataWareHouse/DatabaseProject-ElasticMasterDB/datavault/ExternalTables/HubLocation.sql
+++ DataWareHouse/DatabaseProject-ElasticMasterDB/datavault/ExternalTables/HubLocation.sql
@@ -4,19 +4,14 @@ SET QUOTED_IDENTIFIER ON
 GO
 DROP TABLE IF EXISTS [datavault].[HubLocation] 
 GO
-CREATE TABLE [datavault].[HubLocation](
+CREATE EXTERNAL TABLE [datavault].[HubLocation](
 	[LocationHashKey] [nvarchar](32) NOT NULL,
 	[LoadDate] [datetime2](7) NOT NULL,
 	[SourceSystem] [nvarchar](50) NOT NULL,
     [Location_Code] [nvarchar](100) NOT NULL,
 	[LastSeenDate] [datetime2](7) NOT NULL,
-CONSTRAINT [PK_HubLocation] PRIMARY KEY NONCLUSTERED 
-(
-	[LocationHashKey] ASC
-) ON [PRIMARY],
-CONSTRAINT [UK_HubLocation] UNIQUE NONCLUSTERED 
-(
-	[Location_Code] ASC
-) ON [PRIMARY],
-) ON [PRIMARY]
+)
+    WITH (
+    DATA_SOURCE = [ElasticDWHDBQueryDataSrc]
+    );
 GO
--- DataWareHouse/DatabaseProject-dwhdb-DEV/datavault/Tables/SatLocation.sql
+++ DataWareHouse/DatabaseProject-dwhdb-DEV/datavault/Tables/SatLocation.sql
@@ -2,9 +2,9 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-DROP TABLE IF EXISTS [datavault].[SatLocation] 
+DROP EXTERNAL TABLE [datavault].[SatLocation] 
 GO
-CREATE TABLE [datavault].[SatLocation](
+CREATE EXTERNAL TABLE [datavault].[SatLocation](
 -- Key Area
     [LocationHashKey] [nvarchar](32) NOT NULL, -- Parent Hash Key (Associated Hub / Link Hash Key)
     [LoadDate] [datetime2](7) NOT NULL,        -- Required (Satellite Load Date)
@@ -14,7 +14,7 @@ CREATE TABLE [datavault].[SatLocation](
     [LocationChkSum] [nvarchar](32) NOT NULL,  -- Optional (CheckSum) HashDiff
     [ExtractDate] [datetime2](7) NOT NULL,     -- Optional (Source Load Date)
 -- Descriptive Attributes (very slow frequency) 
-    [SatLocation_Code] [nvarchar](100) NOT NULL,
+    [Location_Code] [nvarchar](100) NOT NULL,
     [Yard_Code] [nvarchar](20) NULL,
 	[Loco_Code] [nvarchar](20) NULL,
 	[Loca_Code] [nvarchar](20) NULL,
@@ -23,15 +23,8 @@ CREATE TABLE [datavault].[SatLocation](
 	[Loco_Name] [nvarchar](100) NULL,
 	[Loca_Name] [nvarchar](100) NULL,
 	[MKTP_Name] [nvarchar](100) NULL
-) ON [PRIMARY]
-GO
-SET ANSI_PADDING ON
-GO
-ALTER TABLE [datavault].[SatLocation] ADD  CONSTRAINT [PK_SatLocation] PRIMARY KEY NONCLUSTERED 
-(
-	[LocationHashKey] ASC
-    ,[LoadDate] ASC
-    ,[SourceSystem] ASC
-
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)
+    WITH (
+    DATA_SOURCE = [ElasticDWHDBQueryDataSrc]
+    );
 GO
