5bda0f990c907150d103c0d47e79db5d193d5434
tvillarreal-key <tvillarreal@keyenergy.com>
2024-05-31 13:09:07 -0500

    location changes

--- DataWareHouse/DatabaseProject-stgdb-DEV/curated/Tables/dimLocation.sql
+++ DataWareHouse/DatabaseProject-stgdb-DEV/curated/Tables/dimLocation.sql
@@ -7,9 +7,8 @@ GO
 CREATE TABLE [curated].[dimLocation](
 	[LocationHashKey] [nvarchar](32) NOT NULL,
 	[LocationChkSum] [nvarchar](32) NOT NULL,
-	[LoadDate] [datetime2](7) NOT NULL,
+	[Location_Code] [nvarchar](500) NOT NULL,
 	[SourceSystem] [nvarchar](50) NOT NULL,
-	[LastSeenDate] [datetime2](7) NULL,
 	[Yard_Code] [nvarchar](20) NULL,
 	[Yard_Name] [nvarchar](100) NULL,
 	[Loco_Code] [nvarchar](20) NULL,
@@ -17,7 +16,8 @@ CREATE TABLE [curated].[dimLocation](
 	[Loca_Code] [nvarchar](20) NULL,
 	[Loca_Name] [nvarchar](100) NULL,
 	[MKTP_Code] [nvarchar](20) NULL,
-	[MKTP_Name] [nvarchar](100) NULL
+	[MKTP_Name] [nvarchar](100) NULL,
+	[LoadDate] [datetime2](7) NOT NULL
 ) ON [PRIMARY]
 GO
  SET ANSI_PADDING ON
--- DataWareHouse/DatabaseProject-stgdb-DEV/curated/Views/vw_distinct_location_recs.sql
+++ DataWareHouse/DatabaseProject-stgdb-DEV/curated/Views/vw_distinct_location_recs.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-DROP VIEW IF EXISTS [curated].[vw_distinct_location_recs] 
+DROP VIEW [curated].[vw_distinct_location_recs]
 GO
 CREATE VIEW [curated].[vw_distinct_location_recs] AS
 SELECT DISTINCT
@@ -25,10 +25,14 @@ SELECT DISTINCT
                     RTRIM(LTRIM(COALESCE(Mktp_Name,'N/A')))
             ))
         ),2
-    )) As LocationChkSum    
-    ,getdate() As LoadDate
+    )) As LocationChkSum  
+        ,UPPER(CONCAT(
+        RTRIM(LTRIM(COALESCE(Yard_Name,'N/A'))), ';',
+        RTRIM(LTRIM(COALESCE(Loco_Name,'N/A'))), ';',
+        RTRIM(LTRIM(COALESCE(Loca_Name,'N/A'))), ';',
+        RTRIM(LTRIM(COALESCE(Mktp_Name,'N/A')))
+    )) Location_Code    
     ,SourceSystem
-    ,NULL As LoadSeenDate
     ,COALESCE(Yard_Code,'N/A') AS Yard_Code
     ,COALESCE(Yard_Name,'N/A') AS Yard_Name
     ,COALESCE(Loco_Code,'N/A') AS Loco_Code
@@ -37,5 +41,7 @@ SELECT DISTINCT
     ,COALESCE(Loca_Name,'N/A') AS Loca_Name
     ,COALESCE(Mktp_Code,'N/A') AS Mktp_Code
     ,COALESCE(Mktp_Name,'N/A') AS Mktp_Name
+    ,getdate() As LoadDate
 From curated.Mdm_Assets
 ;
+GO
