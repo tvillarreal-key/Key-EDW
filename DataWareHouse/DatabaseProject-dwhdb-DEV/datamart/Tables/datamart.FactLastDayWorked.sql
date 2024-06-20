f6061c40adbd381c5a8ad962a4cd6b16a7bf5c32
tvillarreal-key <tvillarreal@keyenergy.com>
2024-06-19 22:16:36 -0500

    FactLastDayWorked

--- DataWareHouse/DatabaseProject-dwhdb-DEV/datamart/Tables/datamart.FactLastDayWorked.sql
+++ DataWareHouse/DatabaseProject-dwhdb-DEV/datamart/Tables/datamart.FactLastDayWorked.sql
@@ -5,15 +5,13 @@ GO
 DROP TABLE IF EXISTS [datamart].[FactLastDayWorked] 
 GO
 CREATE TABLE [datamart].[FactLastDayWorked](
-
        [AssetsHashKey] [nvarchar](32) NOT NULL
        ,[MktPlaceHashKey] [nvarchar](32) NOT NULL
        ,[YardHashKey] [nvarchar](32) NOT NULL
        ,[AssetClassHashKey] [nvarchar](32) NOT NULL
        ,[D365_Status_Code] [nvarchar](20) NULL
        ,[D365_Status_Name] [nvarchar](255) NULL
-       ,[Rig_Status_Code] [nvarchar](10) NULL
-       ,[Status_Match_Flag] [int] NOT NULL
+       ,[Last_Day_Worked_Date][date] NOT NULL
        ,[FactLoadDate] [datetime2](7) NOT NULL
 ) ON [PRIMARY]
 GO
