ac549948c745b8f22fe1632f0f1313004c23ef87
tvillarreal-key <tvillarreal@keyenergy.com>
2024-07-19 17:38:18 -0500

    update assetchksum for distinct dim asset

--- DataWareHouse/DatabaseProject-stgdb-DEV/curated/Views/vw_distinct_dimasset_recs.sql
+++ DataWareHouse/DatabaseProject-stgdb-DEV/curated/Views/vw_distinct_dimasset_recs.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-DROP VIEW IF EXISTS [curated].[vw_distinct_dimasset_recs] 
+DROP VIEW IF EXISTS [curated].[vw_distinct_dimasset_recs]
 GO
 CREATE VIEW [curated].[vw_distinct_dimasset_recs] AS
 SELECT DISTINCT
@@ -15,9 +15,42 @@ SELECT DISTINCT
     )) As AssetsHashKey    
     ,UPPER(CONVERT(char(32), 
         HASHBYTES('MD5',
-            UPPER(
-                    RTRIM(LTRIM(COALESCE(Asset_Desc,'N/A'))
-            ))
+            upper(
+                concat(
+                    trim(coalesce(Asset_Desc, '')),
+                    trim(coalesce(Asset_Class, '')),
+                    trim(coalesce(Manufacturer, '')),
+                    trim(coalesce(Model, '')),
+                    trim(coalesce(Model_Year, '')),
+                    trim(coalesce(Series, '')),
+                    trim(coalesce(Serial_Num, '')),
+                    trim(coalesce(Status, '')),
+                    trim(coalesce(Status_Desc, '')),
+                    trim(coalesce(Source, '')),
+                    coalesce(Marketed, ''),
+                    coalesce(Revenue_Track, ''),
+                    trim(coalesce(Parent_Asset_Num, '')),
+                    trim(coalesce(Child_Relationship_Type, '')),
+                    trim(coalesce(W_Status, '')),
+                    coalesce(Crew_Needed,''), 
+                    coalesce(Avail_Date, '2999-12-31'),
+                    coalesce(LastWT_Date, '2999-12-31'),
+                    trim(coalesce(Note, '')),
+                    trim(coalesce(Customer, '')),
+                    trim(coalesce(JobType, '')),
+                    trim(coalesce(Supervisor, '')),
+                    trim(coalesce(Operator, '')),   
+                    trim(coalesce(Directions, '')),
+                    trim(coalesce(CompanyMan, '')),
+                    trim(coalesce(CompanyManPhone, '')),
+                    trim(coalesce(Daylight24HR, '')),
+                    trim(coalesce(Size, '')),
+                    trim(coalesce(UsedByRigYard, '')),
+                    trim(coalesce(UsedByRigNum, '')),
+                    coalesce(Source_LastUpdateDate, '2999-12-31'),
+                    trim(coalesce(Source_LastUpdateBy, '')) 
+                )
+            )
         ),2
     )) As AssetsChkSum  
     ,COALESCE(Asset_Num,'N/A') AS Asset_Num
@@ -34,28 +67,52 @@ SELECT DISTINCT
     ,COALESCE(Revenue_Track,0) AS Revenue_Track
     ,COALESCE(Parent_Asset_Num,'N/A') AS Parent_Asset_Num
     ,COALESCE(Child_Relationship_Type,'N/A') AS Child_Relationship_Type
-    ,COALESCE(W_Status,'N/A') AS W_Status
+    ,CASE 
+        WHEN [W_Status] = '' OR [W_Status] IS NULL THEN 'N/A'
+        ELSE [W_Status]
+     END AS [W_Status]    
     ,COALESCE(Crew_Needed,0) AS Crew_Needed
     ,COALESCE(Avail_Date,'2999-12-31') AS Avail_Date
     ,COALESCE(LastWT_Date,'2999-12-31') AS LastWT_Date
-    ,COALESCE(Note,'N/A') AS Note
-    ,COALESCE(Customer,'N/A') AS Customer
+    ,CASE 
+        WHEN [Note] = '' OR [Note] IS NULL THEN 'N/A'
+        ELSE [Note]
+     END AS [Note]     
+    ,CASE 
+        WHEN [Customer] = '' OR [Customer] IS NULL THEN 'N/A'
+        ELSE [Customer]
+     END AS [Customer]     
     ,CASE 
         WHEN [JobType] = '' OR [JobType] IS NULL THEN 'N/A'
         ELSE [JobType]
      END AS [JobType]
-    ,COALESCE(Supervisor,'N/A') AS Supervisor
-    ,COALESCE(Operator,'N/A') AS Operator
+    ,CASE 
+        WHEN [Operator] = '' OR [Operator] IS NULL THEN 'N/A'
+        ELSE [Operator]
+     END AS [Operator]  
+    ,CASE 
+        WHEN [Supervisor] = '' OR [Supervisor] IS NULL THEN 'N/A'
+        ELSE [Supervisor]
+     END AS [Supervisor]
     ,COALESCE(Directions,'N/A') AS Directions
     ,COALESCE(CompanyMan,'N/A') AS CompanyMan
     ,COALESCE(CompanyManPhone,'N/A') AS CompanyManPhone
-    ,COALESCE(Daylight24HR,'N/A') AS Daylight24HR
+    ,CASE 
+        WHEN [Daylight24HR] = '' OR [Daylight24HR] IS NULL THEN 'N/A'
+        ELSE [Daylight24HR]
+     END AS [Daylight24HR]     
     ,CASE 
         WHEN [Size] = '' OR [Size] IS NULL THEN 'N/A'
         ELSE [Size]
      END AS [Size]
-    ,COALESCE(UsedByRigYard,'N/A') AS UsedByRigYard
-    ,COALESCE(UsedByRigNum,'N/A') AS UsedByRigNum
+    ,CASE 
+        WHEN [UsedByRigYard] = '' OR [UsedByRigYard] IS NULL THEN 'N/A'
+        ELSE [UsedByRigYard]
+     END AS [UsedByRigYard]
+    ,CASE 
+        WHEN [UsedByRigNum] = '' OR [UsedByRigNum] IS NULL THEN 'N/A'
+        ELSE [UsedByRigNum]
+     END AS [UsedByRigNum]
     ,COALESCE(Source_LastUpdateDate,'2999-12-31') AS Source_LastUpdateDate
     ,COALESCE(Source_LastUpdateBy,'N/A') AS Source_LastUpdateBy
     ,getdate() As LoadDate
