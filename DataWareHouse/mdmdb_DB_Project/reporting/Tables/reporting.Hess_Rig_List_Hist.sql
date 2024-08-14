0468c57c2aec625953c91a9994f3964ba230f7f4
tvillarreal-key <tvillarreal@keyenergy.com>
2024-08-13 20:18:55 -0500

    Added new calculated column to Hess_Rig_ListHist

--- DataWareHouse/mdmdb_DB_Project/reporting/Tables/reporting.Hess_Rig_List_Hist.sql
+++ DataWareHouse/mdmdb_DB_Project/reporting/Tables/reporting.Hess_Rig_List_Hist.sql
@@ -2,11 +2,12 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-DROP PROCEDURE IF EXISTS [reporting].[Hess_Rig_List_Hist]
+DROP TABLE IF EXISTS [reporting].[Hess_Rig_List_Hist]
 GO
 CREATE TABLE [reporting].[Hess_Rig_List_Hist](
 	[RIG] [nvarchar](10) NOT NULL,
 	[RIGTYPE] [nvarchar](20) NOT NULL,
+	[RIGSUBTYPE] [nvarchar](20) NOT NULL,
 	[OPDATE] [date] NOT NULL
 ) ON [PRIMARY]
 GO
