54cfd28c229c16fd0117e9b0a620a367be1622a4
tvillarreal-key <152915628+tvillarreal-key@users.noreply.github.com>
2024-09-26 11:07:40 -0500

    Hess table/view list

---
+++ Data Quality/DQ Profiles/List Tables and Views For Schema.sql
@@ -0,0 +1,15 @@
+DECLARE @table_catalog NVARCHAR(128) = 'mdmdb';
+DECLARE @table_schema NVARCHAR(128) = 'reporting';
+
+SELECT 
+    TABLE_CATALOG AS catalog_name, 
+    TABLE_SCHEMA AS schema_name, 
+    TABLE_NAME AS object_name, 
+    CASE WHEN TABLE_TYPE = 'VIEW' THEN 'VIEW' ELSE 'TABLE' END AS object_type
+FROM 
+    INFORMATION_SCHEMA.TABLES
+WHERE 
+    TABLE_CATALOG = @table_catalog 
+    AND TABLE_SCHEMA = @table_schema
+    AND TABLE_NAME LIKE 'HESS_%'
+ORDER BY 4,3
